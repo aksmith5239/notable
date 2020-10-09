acb4391f79643119e1483ebba8be9df20dee3cb6
aksmith5239 <aklobby@gmail.com>
2020-10-09 13:00:08 -0600

    Add Heroku

--- server.js
+++ server.js
@@ -3,15 +3,29 @@ const app = express();
 const PORT = 3001;
 const { notes } = require('./db/db.json');
 
-
+function filterByQuery(query, notesArray) {
+    let filteredResults = notesArray;
+    if(query.title) {
+        filteredResults = filteredResults.filter(note => note.title === query.title);
+    }
+    if(query.text) {
+        filteredResults = filteredResults.filter(note => note.text === query.text);
+    }
+    // console.log(filteredResults);
+    return filteredResults;
+    
+}
 
 
 
 app.get("/api/notes", (req, res) => {
     let results = notes;
-    console.log(req.query)
-    res.json(notes);
-})
+    // console.log(req.query)
+    if(req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
 //port listener
 app.listen(3001, () => {
     console.log(`API server now on port 3001`);
