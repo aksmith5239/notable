f95ff58a8a0572baae9a538544c3bcaeb603abc4
aksmith5239 <aklobby@gmail.com>
2020-10-10 16:02:03 -0600

    updated files

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
-const {createNewNote} = require('../../lib/notes');
+const {createNewNote, findById, validateNote} = require('../../lib/notes');
 const { notes } = require('../../db/db.json');
-
-
 
 router.get("/notes", (req, res) => {
     let results = notes;
@@ -10,18 +8,21 @@ router.get("/notes", (req, res) => {
     res.json(results);
 });
 
-// // filter by id parameter 
-// router.get('/notes/:id', (req, res) => {
-//     const result = findById(req.params.id, notes);
-//     res.json(result);
-// });
+// filter by id parameter 
+router.get('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    res.json(result);
+});
 
 router.post('/notes', (req, res) => {
     //set id
     req.body.id = notes.length.toString(); 
-    //add note
+   
+    if (!validateNote(req.body)) {
+        res.status(400).send("Please add a title and a note!");
+      } else {
         const note = createNewNote(req.body, notes);
         res.json(note);
-    
+      }    
 });
 module.exports = router;
